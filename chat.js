43ab09d357955424667f838a1f0a1b10099bdb9e
minvinaa <114900423+minvinaa@users.noreply.github.com>
2023-11-14 14:28:05 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -2,8 +2,19 @@
 //ADD YOUR FIREBASE LINKS HERE
 
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+    apiKey: "AIzaSyDKhI8qr8zI5xrNMMSTwOg2HwXFZqQ9LXg",
+    authDomain: "lets-chat-54edd.firebaseapp.com",
+    projectId: "lets-chat-54edd",
+    storageBucket: "lets-chat-54edd.appspot.com",
+    messagingSenderId: "208012962328",
+    appId: "1:208012962328:web:4a2bf8ad934bb16104b16d",
+    measurementId: "G-6401JXDGZR"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 function addUser() {
